6bec13a3442f7ab2ebb705f6baf7c05059ce43ff
mwilleke <mwilleke3@gmail.com>
2014-09-10 11:22:57 -0500

    removed onboot broadcastreceiver and onboot code so now will start service strictly from bluetooth

--- app/src/main/java/keystone_technologies/com/readingthecity/BluetoothStateReceiver.java
+++ app/src/main/java/keystone_technologies/com/readingthecity/BluetoothStateReceiver.java
@@ -1,17 +1,9 @@
 package keystone_technologies.com.readingthecity;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-
-import java.util.List;
 
 public class BluetoothStateReceiver extends BroadcastReceiver {
 
@@ -20,8 +12,6 @@ public class BluetoothStateReceiver extends BroadcastReceiver {
         if ("android.bluetooth.adapter.action.STATE_CHANGED".equals(intent.getAction())){
             final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
                     BluetoothAdapter.ERROR);
-           // Notification.Builder builder = new Notification.Builder(context);
-          //  if (Utilities.isBlueToothEnabled()) {
 
             switch (state) {
                 case BluetoothAdapter.STATE_OFF:
@@ -32,26 +22,9 @@ public class BluetoothStateReceiver extends BroadcastReceiver {
                 case BluetoothAdapter.STATE_ON:
                     Utilities.startService(context);
                     Utilities.startTrackingService(context);
-                    //Intent listenerIntent = new Intent(context, InvisibleActivity.class);
-                    //listenerIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    //context.startActivity(listenerIntent);
                     Utilities.postNotification("bluetooth", context);
                     break;
             }
-
-
-
-
-            //  .setAutoCancel(true)
-            //  .setContentIntent(pendingIntent)
-            //  .build();
-            //notification.defaults |= Notification.DEFAULT_SOUND;
-            //notification.defaults |= Notification.DEFAULT_LIGHTS;
-            //notificationManager.notify(Constants.NOTIFICATION_ID, notification);
-
-//            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//            notificationManager.notify(1, builder.build());
-
         }
     }
 }
