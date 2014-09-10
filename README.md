3816ce6de6e75a76cba89ebc1bb17c0542333211
mwilleke <mwilleke3@gmail.com>
2014-09-10 12:17:03 -0500

    added notes in readme for database.  altered main activity to check if bluetooth is enabled and if yes, then start services.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 hi
 world
+
+service table with uuid-major-minor is the beacon and name of field and primary key and not autoincrement
+second field in service table yes/no bool tiny int size 1  0 or 1
+
+
+history table  primary key of id and auto increment
+next field is beacon is foreign key which reaches to the primary key
+thrid field timestamp and define as timestamp -> date and time in seconds
+
+
+settings page:
+
+start and stop service and view service status
+possibly scan time
+yes or no in settings page as well.
--- app/src/main/java/keystone_technologies/com/readingthecity/MainActivity.java
+++ app/src/main/java/keystone_technologies/com/readingthecity/MainActivity.java
@@ -1,69 +1,16 @@
 package keystone_technologies.com.readingthecity;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 
 public class MainActivity extends Activity {
-
-    private BeaconManager beaconManager;
-    private NotificationManager notificationManager;
-   // private Region region;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        if (!Utilities.isBlueToothEnabled()) {
-//            // Bluetooth not enabled, so prompt the user to turn BlueTooth on
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBtIntent, Constants.REQUEST_ENABLE_BT);
-//        }
-//        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-
-        // Configure BeaconManager.
-        beaconManager = new BeaconManager(this);
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override
-            public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
-                // Note that results are not delivered on UI thread.
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Note that beacons reported here are already sorted by estimated
-                        // distance between device and beacon.
-                        getActionBar().setSubtitle("Found beacons: " + beacons.size());
-                        //postNotification("Found beacon!");
-                    }
-                });
-            }
-        });
-
-
-
     }
 
 
@@ -90,6 +37,9 @@ public class MainActivity extends Activity {
     protected void onStart() {
         super.onStart();
 
-   //     connectToService();
+        if (Utilities.isBlueToothEnabled()) {
+            Utilities.startService(this);
+            Utilities.startTrackingService(this);
+        }
     }
 }
