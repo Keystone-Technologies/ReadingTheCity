722924558a4686169678879800c5d4c2002b7e09
mwilleke <mwilleke3@gmail.com>
2014-09-10 20:55:03 -0500

    Beacon class for the Beacon object for database

---
+++ app/src/main/java/keystone_technologies/com/readingthecity/Beacon.java
@@ -0,0 +1,23 @@
+package keystone_technologies.com.readingthecity;
+
+public class Beacon {
+
+    private String UUID;
+    private int response;
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public int getResponse() {
+        return response;
+    }
+
+    public void setResponse() {
+        this.response = response;
+    }
+}
