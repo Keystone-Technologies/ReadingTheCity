46288e385b70a810c8845a49fc7f4d3b9147e275
mwilleke <mwilleke3@gmail.com>
2014-09-13 14:02:58 -0500

    adding yes no constants to update file

--- app/src/main/java/keystone_technologies/com/readingthecity/Constants.java
+++ app/src/main/java/keystone_technologies/com/readingthecity/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String EXTRAS_BEACON = "extrasBeacon";
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
+    public static final int YES = 1;
+    public static final int NO = 0;
 }
