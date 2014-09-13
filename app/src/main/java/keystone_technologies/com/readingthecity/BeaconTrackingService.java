ce20e11265869eb56b88c57867d2c77560df6d94
mwilleke <mwilleke3@gmail.com>
2014-09-13 10:11:51 -0500

    updated the notification when starting service so no longer using deprecated methods

--- app/src/main/java/keystone_technologies/com/readingthecity/BeaconTrackingService.java
+++ app/src/main/java/keystone_technologies/com/readingthecity/BeaconTrackingService.java
@@ -1,7 +1,9 @@
 package keystone_technologies.com.readingthecity;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -36,14 +38,11 @@ public class BeaconTrackingService extends Service {
 
         beaconManager = new BeaconManager(this);
 
-
-
         Notification notification = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.beacon_gray)
-                .setContentText("Looking For Beacons")
-                .setContentTitle("Tracking Service Started")
-                .getNotification();
-
+        .setSmallIcon(R.drawable.beacon_gray)
+        .setContentText("Searching For Beacons")
+        .setContentTitle(getString(R.string.app_name))
+        .build();
 
         startForeground(1, notification);
     }
