5617e5df72b6da1937c7908aeeaec20d125c47cd
mwilleke <mwilleke3@gmail.com>
2014-09-14 11:11:52 -0500

    added a response listener file for yes no questions.

---
+++ app/src/main/java/keystone_technologies/com/readingthecity/ResponseListener.java
@@ -0,0 +1,13 @@
+package keystone_technologies.com.readingthecity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ResponseListener extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
